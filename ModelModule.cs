2809313f983057eb008b8d70fd166a2d58bc6ccd
Nicolai Sattler <nicolaisattler1990@gmail.com>
2017-11-12 13:42:31 +0100

    develop: added autofac

--- ModelModule.cs
+++ ModelModule.cs
@@ -2,8 +2,20 @@ using Autofac;
 
 namespace CodeSquirl.Recipy.Model
 {
-    public class ModelModule : Modele
+    public class ModelModule : Module
     {
-
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<Day>();
+            builder.RegisterType<GroceryList>();
+            builder.RegisterType<Meal>();
+            builder.RegisterType<Ingredient>();
+            builder.RegisterType<Necessity>();
+            builder.RegisterType<Product>();
+            builder.RegisterType<Unit>();
+            builder.RegisterType<Preparation>();
+            builder.RegisterType<Recipy>();
+            builder.RegisterType<InstructionBase>();
+        }
     }
 }
