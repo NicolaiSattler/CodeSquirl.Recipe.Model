ac98bf24fe6847c5e2675adaf36acd468672e952
Nicolai Sättler <n.sattler@dataquint.nl>
2019-01-29 19:55:01 +0100

    develop | add UniqueID to domain object

--- src/Interface/IProduct.cs
+++ src/Interface/IProduct.cs
@@ -5,6 +5,7 @@ namespace CodeSquirl.RecipeApp.Model
 {
     public interface IProduct
     {
+        Guid UniqueID { get; set; }
         string Name { get; set; }
         ProductType Type { get; set; }
         bool Perishable { get; set; }
--- src/Model/Product/Product.cs
+++ src/Model/Product/Product.cs
@@ -5,6 +5,7 @@ namespace CodeSquirl.RecipeApp.Model
 {
     public class Product : IProduct
     {
+        public Guid UniqueID { get; set; }
         public string Name { get; set; }
         public ProductType Type { get; set; }
         public bool Perishable { get; set; }
